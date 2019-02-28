1621dae43a5a64c3d17c2cab7e19f3be5a0f8a97
David Dai <stdioa@163.com>
2019-02-28 11:26:22 +0800

    Add build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#COMPILER AND PATH
+PREFIX=/usr
+C_COMPILER="gcc"
+CXX_COMPILER="g++"
+
+## BUILD ##
+PKG_CONFIG_PATH=/opt/aria2/build_libs/lib/pkgconfig/ \
+LD_LIBRARY_PATH=/opt/aria2/build_libs/lib/ \
+CC="$C_COMPILER" \
+CXX="$CXX_COMPILER" \
+./configure \
+    --prefix=$PREFIX \
+    --enable-https \
+    --enable-bittorrent \
+    --enable-gzip \
+    --enable-metalink \
+    --enable-xmlrpc \
+    && make -j4
